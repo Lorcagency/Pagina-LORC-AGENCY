4db3766521567f779edcc28d5533943f4c433b3b
Emiliano Ortega Castellanos <emilianoortegacastellanos@Emiliano-MacBook-Air.local>
2026-04-26 23:46:08 -0600

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
+import { useEffect } from 'react'
 import Home from './pages/Home'
 import Contacto from './pages/Contacto'
 import QuienesSomos from './pages/QuienesSomos'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
+
 export default function App() {
   const location = useLocation()
   return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Home />} />
-        <Route path="/contacto" element={<Contacto />} />
-        <Route path="/quienes-somos" element={<QuienesSomos />} />
-      </Routes>
-    </AnimatePresence>
+    <>
+      <ScrollToTop />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/quienes-somos" element={<QuienesSomos />} />
+        </Routes>
+      </AnimatePresence>
+    </>
   )
 }
 
